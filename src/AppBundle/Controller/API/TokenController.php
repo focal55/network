5600adfa9b035799e85faac0360de29c59ee5f21
Joe Ybarra <joe@thinkbean.com>
2017-01-08 00:21:32 -0500

    Finished Tests for JWT token generation

--- src/AppBundle/Controller/API/TokenController.php
+++ src/AppBundle/Controller/API/TokenController.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ybarra
- * Date: 1/4/17
- * Time: 1:19 PM
- */
 
 namespace AppBundle\Controller\API;
 
-
+use AppBundle\Api\ApiProblem;
+use AppBundle\Api\ApiProblemException;
 use AppBundle\Controller\BaseController;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
 use Facebook\Facebook;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -17,6 +14,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use UserBundle\Entity\User;
 
 class TokenController extends BaseController
 {
@@ -25,47 +23,87 @@ class TokenController extends BaseController
      * @Route("/api/tokens/{op}")
      * @Method("POST")
      */
-    public function newTokenAction(Request $request, $op)
+    public function newTokenAction(Request $request, $op = 'credentials')
     {
-        $token = NULL;
+
+        $create_new_user = FALSE;
 
         // Check if the user's email is in the database.
         $user = $this->getDoctrine()
             ->getRepository('UserBundle:User')
-            ->findOneBy(['email' => $request->getUser()]);
-
-        if (!$user) {
-            throw $this->createNotFoundException('No User');
-        }
+            ->findOneBy(['email' => $request->get('email')]);
 
         // If $op is fb, attempt to use the access token to access their profile.
-        if ($op == 'fb' && isset($request['fb_token'])) {
-            $fb = new Facebook([
-                'app_id' => $this->container->getParameter('fb_app_id'),
-                'app_secret' => $this->container->getParameter('fb_secret'),
-            ]);
-            $check_response = $fb->get('/me', $request['fb_token']);
-            if ($check_response[])
-        }
-        // If $op is credentials, check password.
-        elseif ($op == 'credentials') {
-            $isValid = $this->get('security.password_encoder')
-                ->isPasswordValid($user, $request->getPassword());
-            if (!$isValid) {
+        if ($op == 'fb') {
+            if ($request->get('fb_token')) {
+                $fb = new Facebook([
+                    'app_id' => $this->container->getParameter('fb_app_id'),
+                    'app_secret' => $this->container->getParameter('fb_secret'),
+                ]);
+
+                try {
+                    $response = $fb->get('/me?fields=email,name', $request->get('fb_token'));
+                    // FB User Found, if no user in db, create it.
+                    if (!$user) {
+                        $fb_user = $response->getGraphUser();
+                        $user = new User();
+                        $user->setEmail($fb_user['email']);
+                        $user->setCreated(time());
+                        $user->setUpdated(time());
+                        $user->setPlainPassword($this->generatePassword());
+                        $em = $this->getDoctrine()->getManager();
+                        $em->persist($user);
+                        $em->flush();
+                    }
+
+                } catch(FacebookResponseException $e) {
+                    $apiProblem = new ApiProblem(400, ApiProblem::TYPE_VALIDATION_ERROR);
+                    $apiProblem->set('details', $e->getMessage());
+                    return $this->get('api.response_factory')->createResponse($apiProblem);
+
+                } catch(FacebookSDKException $e) {
+                    $apiProblem = new ApiProblem(400, ApiProblem::TYPE_VALIDATION_ERROR);
+                    $apiProblem->set('details', $e->getMessage());
+                    return $this->get('api.response_factory')->createResponse($apiProblem);
+                }
+            }
+            else {
                 throw new BadCredentialsException();
             }
         }
 
+        // If $op is credentials, check password.
+        elseif ($op == 'credentials' && $user) {
+            $isValid = $this->get('security.password_encoder')
+                ->isPasswordValid($user, $request->get('password'));
+            if (!$isValid) {
+                throw new BadCredentialsException();
+            }
+        }
+        elseif (!$user) {
+            throw new BadCredentialsException();
+        }
 
-
-
-
+        // Generate JWT.
         $token = $this->get('lexik_jwt_authentication.encoder')
             ->encode(['username' => $user->getUsername()]);
 
         return new JsonResponse([
-            'token' => $token
+            'token' => $token,
+            'create_new_user' => $create_new_user
         ]);
+    }
 
+    private function generatePassword($length = 8)
+    {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $count = mb_strlen($chars);
+
+        for ($i = 0, $result = ''; $i < $length; $i++) {
+            $index = rand(0, $count - 1);
+            $result .= mb_substr($chars, $index, 1);
+        }
+
+        return $result;
     }
 }
--- tests/AppBundle/Controller/API/TokenControllerTest.php
+++ tests/AppBundle/Controller/API/TokenControllerTest.php
@@ -18,7 +18,10 @@ class TokenControllerTest extends ApiTestCase
         $this->createUser('weaverryan', 'test');
 
         $response = $this->client->post('/api/tokens', [
-            'auth' => ['weaverryan@foo.com', 'test']
+            'form_params' => [
+                'email' => 'weaverryan@foo.com',
+                'password' => 'test'
+            ]
         ]);
         $this->assertEquals(200, $response->getStatusCode());
         $this->asserter()->assertResponsePropertyExists(
@@ -32,8 +35,45 @@ class TokenControllerTest extends ApiTestCase
         $this->createUser('weaverryan', 'testasdsad');
 
         $response = $this->client->post('/api/tokens', [
-            'auth' => ['weaverryan', 'test']
+          'form_params' => [
+            'email' => 'weaverryan@foo.com',
+            'password' => 'test'
+          ]
         ]);
         $this->assertEquals(401, $response->getStatusCode());
+    }
+
+    public function testPOSTCreateTokenWithFBAuthOnExistingUser()
+    {
+        $this->createUser('joe', 'test');
+
+        $response = $this->client->post('/api/tokens/fb', [
+          'form_params' => [
+            'email' => 'joe@foo.com',
+            'fb_token' => 'EAACYSNVMJkcBAJRfED83K5ENpNDDYBscggP2nPxnLXIaK0hi101wh1TghAv9EsPUjQUF7364B9HSgDGZB9xWdyKXPHB4TVa2dMmoDZCydEZCuu1QZAPCrYmrPk2iLvPN6ZC4SZA6NPFStiU4eMDf3ow4IIJflO1z8tdXznxeENYQZDZD'
+          ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->asserter()->assertResponsePropertyExists(
+          $response,
+          'token'
+        );
+    }
+
+    public function testPOSTCreateTokenWithFBAuthOnNonExistingUser()
+    {
+        $response = $this->client->post('/api/tokens/fb', [
+          'form_params' => [
+            'email' => 'nouser@foo.com',
+            'fb_token' => 'EAACYSNVMJkcBAJRfED83K5ENpNDDYBscggP2nPxnLXIaK0hi101wh1TghAv9EsPUjQUF7364B9HSgDGZB9xWdyKXPHB4TVa2dMmoDZCydEZCuu1QZAPCrYmrPk2iLvPN6ZC4SZA6NPFStiU4eMDf3ow4IIJflO1z8tdXznxeENYQZDZD'
+          ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->asserter()->assertResponsePropertyExists(
+          $response,
+          'token'
+        );
     }
 }
